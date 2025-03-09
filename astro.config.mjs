9d4dd2a6605d2dbe8f756468f485980731c55256
sakadume masato <namonakisizin@gmail.com>
2025-03-10 00:24:57 +0900

    [add] 設定値を元に戻す

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: process.env.SITE_URL || 'http://localhost:4321',
-  output: 'static',
-  adapter: cloudflare(),
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
 });
