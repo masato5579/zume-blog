1fdf673139d6df42f0e6396eec6a2ff85ebe23d9
sakadume masato <namonakisizin@gmail.com>
2025-03-09 19:14:04 +0900

    [add] サイトのURLを環境変数で管理するように修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  site: process.env.SITE_URL || 'http://localhost:4321',
   output: 'server',
   adapter: cloudflare(),
 });
