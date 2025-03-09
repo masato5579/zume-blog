74b0046835fda52d8cbd221941ebcbc68dea7aa8
sakadume masato <namonakisizin@gmail.com>
2025-03-09 21:22:06 +0900

    [fix] mode:directoryを削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: process.env.SITE_URL || 'http://localhost:4321',
   output: 'static',
-  adapter: cloudflare({
-    mode: 'directory',
-  }),
+  adapter: cloudflare(),
 });
